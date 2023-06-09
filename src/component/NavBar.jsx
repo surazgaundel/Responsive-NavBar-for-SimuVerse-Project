826a328ca2caa8e15d60aebf6cbda99b88904297
surazgaundel <gaundelsuraz99@gmail.com>
2023-06-09 14:37:34 +0530

    Mobile view commit

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState} from 'react'
 import  Logo from '../assets/Logo.svg';
 import  MobileLogo from '../assets/MobileLogo.svg';
 import Search from '../assets/search.svg'
@@ -7,45 +7,43 @@ import user from '../assets/user.svg'
 import wishlist from '../assets/wishlist.svg'
 import { NavLink } from 'react-router-dom';
 import {GiHamburgerMenu} from 'react-icons/gi';
+import {RxCross2} from 'react-icons/rx';
 import Links from '../DataTab.js';
 
 export default function NavBar() {
-    // const [burgerClass,setBurgerClass]=useState('burger-bar unclicked')
-    // const [menuClass,setMenuClass]=useState('menu hidden');
-    // const [isMenuClicked, setIsMenuClicked] = useState(false)
+    const [isOpen, setOpen]=useState(false);
 
-
-    // const updateMenu = () => {
-    //     if(!isMenuClicked) {
-    //         setBurgerClass("burger-bar clicked")
-    //         setMenuClass("menu visible")
-    //     }
-    //     else {
-    //         setBurgerClass("burger-bar unclicked")
-    //         setMenuClass("menu hidden")
-    //     }
-    //     setIsMenuClicked(!isMenuClicked)
-    // }
-
-
+    const openBarMenu=()=>{
+        setOpen(true);
+        console.log(true)
+    }
+    const closeBarMenu=()=>{
+        setOpen(false);
+        console.log(false)
+    }
   return (
    <nav className="navbar-container">
         <div className="header-container">
             <div className="logo-container">
                 <NavLink to='/'>
-                <img src={Logo} alt='logo'/>
+                <img src={Logo} alt='logo' className='web-logo'/>
+                <img src={MobileLogo} alt='logo' className='mobile-logo'/>
                 </NavLink>
             </div>
-            <div className="burger-container">
+            <button className={`burger-container ${isOpen?'isOpen':''}`} onClick={openBarMenu}>
                 <GiHamburgerMenu/>
-            </div>
-            <div className='tabs-container'>
+            </button>
+
+            <div className={`tabs-container ${isOpen?'isOpen':''}`}>
+                <button className="cross-container" onClick={closeBarMenu}>
+                    <RxCross2/>
+                </button>
                 {Links.map((link,index)=>{
                     return( <NavLink key={index} to={link.to} className='link'>{link.name}</NavLink>)
                 })}
             </div>
             <div className='symbol-user-container'>
-            <div className='symbol-container'>
+            <div className={`symbol-container ${isOpen?'isOpen':''}`}>
                 <div><NavLink to='/search'><img src={Search} alt='search-button'/></NavLink></div><div className='vertical-line'></div>
                 <div><NavLink to='/wishlist'><img src={wishlist} alt='wishlist-button'/></NavLink></div><div className='vertical-line'></div>
                 <div><NavLink to='/cart'><img src={shoppingBag} alt='shoppingBag-button'/></NavLink></div><div className='vertical-line'></div>
